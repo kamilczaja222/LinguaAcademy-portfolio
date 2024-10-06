796aebaeecfae3822aef34b4accdd395f43068b2
kamilczaja222 <kamilczaja222@gmail.com>
2024-10-06 16:34:58 +0200

    Hide button for scroll - corousel post

--- src/main.js
+++ src/main.js
@@ -2,24 +2,53 @@ const prevButton = document.querySelector('.js-carousel-prev');
 const nextButton = document.querySelector('.js-carousel-next');
 const opinionBox = document.querySelector('.js-carousel-opinions');
 const carouselBox = document.querySelector('.js-carousel-box');
+const screenBox = document.querySelector('.js-screen-box');
 const hamburger = document.querySelector('.js-hamburger');
 const nav = document.querySelector('.js-nav');
 
-let element = 2;
+let counter = 0;
+let maxScroll = 0;
 
-const margin = (carouselBox.offsetWidth - opinionBox.offsetWidth)/element;
+const screenSize = parseInt(screenBox.offsetWidth);
+let position = parseInt(carouselBox.offsetWidth)
+if(screenSize < 768){
+    position = position;
+    maxScroll = 5;
+} else if(screenSize > 768 && screenSize < 1256) {
+    position = 0.5*position;
+    maxScroll = 5;
+} else {
+    position = 0.33*position;
+    maxScroll = 5;
+    maxScroll = 3;
+}
+
 
 
 nextButton.addEventListener('click', () => {
     const position = parseInt(opinionBox.offsetWidth);
     carouselBox.scrollLeft += position;
-    console.log(position);
+    counter++;
+    console.log(counter);
+    if(counter > 0) {
+        prevButton.classList.remove('js-hidden');
+    }
+    if(counter >=maxScroll) {
+        nextButton.classList.add('js-hidden');
+    }
 })
 
 prevButton.addEventListener('click', () => {
     const position = opinionBox.offsetWidth;
     carouselBox.scrollLeft -= position;
-    console.log(position);
+    counter--;
+    console.log(counter);
+    if(counter <= maxScroll) {
+        nextButton.classList.remove('js-hidden');
+    }
+    if(counter <= 0) {
+        prevButton.classList.add('js-hidden');
+    }
 })
 
 hamburger.addEventListener('click', () => {
