ab1ecff8bd8eb83c5f0d134eaff44f2dcf05e8ee
kamilczaja222 <46896993+kamilczaja222@users.noreply.github.com>
2024-10-07 18:18:43 +0200

    JS refactor after pull request

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+const HIDDEN_CLASS = 'js-hidden';
 const prevButton = document.querySelector('.js-carousel-prev');
 const nextButton = document.querySelector('.js-carousel-next');
 const opinionBox = document.querySelector('.js-carousel-opinions');
@@ -6,12 +7,12 @@ const screenBox = document.querySelector('.js-screen-box');
 const hamburger = document.querySelector('.js-hamburger');
 const nav = document.querySelector('.js-nav');
 
-let counter = 0;
+let carouselPostCounter = 0;
 let maxScroll = 0;
-const screenSize = parseInt(screenBox.offsetWidth);
+let screenSize;
 const position = parseInt(opinionBox.offsetWidth)
 
-function checkScreen(screenSize) {
+function checkScreen() { 
     if(screenSize < 768) {
         maxScroll = 5;
     } else if (screenSize < 1256) {
@@ -19,33 +20,38 @@ function checkScreen(screenSize) {
     } else {
         maxScroll = 3;
     }
+    console.log('resize')
 }
 
-checkScreen(screenSize);
+document.addEventListener("DOMContentLoaded", (event) => {
+    screenSize = parseInt(screenBox.offsetWidth);
+    checkScreen(screenSize);
+});
+
+window.addEventListener("resize", () => {
+    screenSize = parseInt(screenBox.offsetWidth);
+    checkScreen(screenSize);
+});
 
 nextButton.addEventListener('click', () => {
-    checkScreen(screenSize);
     carouselBox.scrollLeft += position;
-    counter++;
-    console.log(counter);
-    if(counter > 0) {
-        prevButton.classList.remove('js-hidden');
+    carouselPostCounter++;
+    if(carouselPostCounter > 0) {
+        prevButton.classList.remove(HIDDEN_CLASS);
     }
-    if(counter >=maxScroll) {
-        nextButton.classList.add('js-hidden');
+    if(carouselPostCounter >=maxScroll) {
+        nextButton.classList.add(HIDDEN_CLASS);
     }
 })
 
 prevButton.addEventListener('click', () => {
-    checkScreen(screenSize);
     carouselBox.scrollLeft -= position;
-    counter--;
-    console.log(counter);
-    if(counter <= maxScroll) {
-        nextButton.classList.remove('js-hidden');
+    carouselPostCounter--;
+    if(carouselPostCounter <= maxScroll) {
+        nextButton.classList.remove(HIDDEN_CLASS);
     }
-    if(counter <= 0) {
-        prevButton.classList.add('js-hidden');
+    if(carouselPostCounter <= 0) {
+        prevButton.classList.add(HIDDEN_CLASS);
     }
 })
 
